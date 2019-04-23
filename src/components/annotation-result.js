c395b8133cae299ea80556510a859d039ccd2e60
enku2015 <enku.wende@gmail.com>
2019-04-23 05:38:12 -0500

    Update Port Assignment

--- src/components/annotation-result.js
+++ src/components/annotation-result.js
@@ -8,7 +8,7 @@ import "antd/dist/antd.css";
 
 export const SERVER_ADDRESS = process.env.SERVICE_ADDR
   ? `http://${process.env.SERVICE_ADDR}:3004`
-  : "http://localhost:3200";
+  : "http://localhost:3004";
 
 export const AnnotationStatus = {
   ACTIVE: 1,
